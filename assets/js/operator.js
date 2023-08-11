ae3bbf8bebc2747613a4a68f62b57844c986d3fe
agungsolihin <mohagungsolihin@gmail.com>
2023-08-11 20:36:03 +0700

    #4 section/pokemon penambahan pokemon.html

---
+++ assets/js/operator.js
@@ -0,0 +1,29 @@
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+  {
+    const name = "Muhammad Agung Solihin";
+    let age = 10;
+
+    if (age >= 18) {
+        console.log("Silahkan masuk")
+    } else {
+        console.log("anda dibawah umur")
+    }
+  }
+
+  {
+    for (i = 0; i = 100; i++) {
+        if (i % 2)
+    }
+
+  }
